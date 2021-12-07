0f5622e2bac7a4b243a6efd58ed8d92544ff65b7
Roland Szép <szep.roland.2000@gmail.com>
2021-12-07 13:22:24 +0100

    Initial Commit

---
+++ alcoholcalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  alcoholcalculator
+//
+//  Created by Szép Roland on 2021. 12. 07..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
